976cd6b17714e51ff767e237bef62ed8ff9da6ed
Ameer Hamza Khetran <hamzaa803@gmail.com>
2023-09-28 04:02:00 +0500

    update: configure langchain

--- src/components/promptGen.tsx
+++ src/components/promptGen.tsx
@@ -1,3 +1,9 @@
+import { OpenAI } from "langchain/llms/openai";
+
+const llm = new OpenAI({
+  openAIApiKey: process.env.OpenAIApiKey,
+});
+
 export default function PromptGeneration() {
     return (
         <p className="text-red-700 text-2xl font-bold">Hello Prompt Generation</p>
