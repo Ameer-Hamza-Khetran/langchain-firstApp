59d7d6a26f658ea8c2e1e0773b93d7f0653fbc3b
Ameer Hamza Khetran <hamzaa803@gmail.com>
2023-09-29 02:10:57 +0500

    update: langchain predictMessage

--- src/components/promptGenPredictMessage.tsx
+++ src/components/promptGenPredictMessage.tsx
@@ -1,5 +1,6 @@
 import { OpenAI } from "langchain/llms/openai";
 import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanMessage } from "langchain/schema";
 
 const llm = new OpenAI({
   openAIApiKey: process.env.OpenAIApiKey,
@@ -13,18 +14,20 @@ const chatModel = new ChatOpenAI({
 
 const text = "What would be a good company name for a company that makes colorful socks?"
 
-const llmResult = await llm.predict(text);
+const messages = [new HumanMessage({ content: text })];
 
-const chatModelResult = await chatModel.predict(text);
+const llmResult = await llm.predictMessages(messages);
+
+const chatModelResult = await chatModel.predictMessages(messages);
 
 export default function PromptGenerationPredictMessage() {
   console.log(llmResult);
   console.log(chatModelResult);
     return (
       <div>
-        <p className="text-red-700 text-2xl font-bold">{`${llmResult}`}</p>
+        <p className="text-red-700 text-2xl font-bold">{`${llmResult.content}`}</p>
         <br/>
-        <p className="text-red-700 text-2xl font-bold">{`${chatModelResult}`}</p>
+        <p className="text-red-700 text-2xl font-bold">{`${chatModelResult.content}`}</p>
       </div>
     );
 }
